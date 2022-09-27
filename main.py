56b36f45561c63e04aa07b9bf8e2c5484c50d85e
eomgerm <fishbread00@gmail.com>
2022-09-27 18:21:02 +0900

    scrape basic infomation

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ if res.status_code != 200:
 else:
     soup = BeautifulSoup(res.text, "html.parser")
     jobs = soup.find_all('section', class_="jobs")
+    for job_section in jobs:
+        job_posts = job_section.find_all('li')
+        job_posts.pop(-1)
+        for post in job_posts:
+            anchors = post.find_all('a')
+            anchor = anchors[1]
+            link = anchor['href']
+            company, kind, region = anchor.find_all('span', class_='company')
+            title = anchor.find('span', class_='title')
