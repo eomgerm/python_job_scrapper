e37e063def79d6bd4e4313242bdedd2492457664
eomgerm <fishbread00@gmail.com>
2022-10-01 16:20:55 +0900

    open_browser function added

--- extractors/indeed.py
+++ extractors/indeed.py
@@ -4,12 +4,16 @@ from extractors.wwr import extract_wwr_jobs
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-def extract_indeed_jobs(keyword):
+def open_browser():
     options = Options()
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
 
     browser = webdriver.Chrome(options=options)
+    return browser
+
+def extract_indeed_jobs(keyword):
+    browser = open_browser()
 
     base_url = 'https://kr.indeed.com/jobs?q='
     search_term = keyword
