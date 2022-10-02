66ef2a9fc911d121e31e71ae545d2b354af6cfa9
eomgerm <fishbread00@gmail.com>
2022-10-02 14:05:21 +0900

    refactoring saving file

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 from operator import imod
 from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
+from file import save_to_file
 
 keyword = input("What do you want to search for?")
 
-file = open(f"{keyword}.csv", "w")
-
-file.write("Position,Company,Location,URL\n")
-
 indeed = extract_indeed_jobs(keyword)
 wwr = extract_wwr_jobs(keyword)
-
 jobs = indeed + wwr
 
-for job in jobs:
-    file.write(f"{job['position']},{job['company']},{job['location']},{job['link']}\n")
-
-file.close()
+save_to_file(keyword, jobs)
