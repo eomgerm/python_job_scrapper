c356dc459300839528ffe52171bf9f4d5a0dd6f2
eomgerm <fishbread00@gmail.com>
2022-10-02 14:43:40 +0900

    rendering search

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from extractors.indeed import extract_indeed_jobs
+from extractors.wwr import extract_wwr_jobs
 
 app = Flask("JobsScrapper")
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/search")
+def search():
+    keyword = request.args.get("keyword")
+    
+    indeed = extract_indeed_jobs(keyword)
+    wwr = extract_wwr_jobs(keyword)
+    jobs = indeed + wwr
+
+
+    return render_template("search.html", keyword=keyword, jobs=jobs)
 
 app.run("127.0.0.1", debug=True)
