fcbcb3acf566cd8c9c876d343878af4a49797835
eomgerm <fishbread00@gmail.com>
2022-10-02 14:00:11 +0900

    write to csv file

---
+++ main.py
@@ -0,0 +1,19 @@
+from operator import imod
+from extractors.indeed import extract_indeed_jobs
+from extractors.wwr import extract_wwr_jobs
+
+keyword = input("What do you want to search for?")
+
+file = open(f"{keyword}.csv", "w")
+
+file.write("Position,Company,Location,URL\n")
+
+indeed = extract_indeed_jobs(keyword)
+wwr = extract_wwr_jobs(keyword)
+
+jobs = indeed + wwr
+
+for job in jobs:
+    file.write(f"{job['position']},{job['company']},{job['location']},{job['link']}\n")
+
+file.close()
